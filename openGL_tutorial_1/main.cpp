36daf6adef6319096cb55a122088e947920984e6
Ahuang <zoe.ahuang@gmail.com>
2017-05-31 10:33:53 -0700

    Initial Commit

---
+++ openGL_tutorial_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL_tutorial_1
+//
+//  Created by Ah Huang on 5/31/17.
+//  Copyright © 2017 Ah Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

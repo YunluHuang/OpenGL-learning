2868e7577d56dde12c39cccedb7e916a0049d8ea
Ahuang <zoe.ahuang@gmail.com>
2017-06-10 21:07:41 -0700

    add Skybox class

---
+++ source/extern/Skybox.hpp
@@ -0,0 +1,14 @@
+//
+//  Skybox.hpp
+//  CSE163-final
+//
+//  Created by Ah Huang on 6/10/17.
+//  Copyright © 2017 Ah Huang. All rights reserved.
+//
+
+#ifndef Skybox_hpp
+#define Skybox_hpp
+
+#include <stdio.h>
+
+#endif /* Skybox_hpp */
---
+++ source/intern/Skybox.cpp
@@ -0,0 +1,9 @@
+//
+//  Skybox.cpp
+//  CSE163-final
+//
+//  Created by Ah Huang on 6/10/17.
+//  Copyright © 2017 Ah Huang. All rights reserved.
+//
+
+#include "Skybox.hpp"
